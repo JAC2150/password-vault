ada88faaa32b2664d7a94c622053d7e28333d147
Joshua Chongo <jac.2150@gmail.com>
2017-10-04 11:40:52 -0400

    finished script

--- password_vault.sh
+++ password_vault.sh
@@ -5,24 +5,48 @@ clear
 	read -p "Enter the password : " password
 
 		if [ $password = Fin123 ] ; then
-			echo " Finace Logins "
+			echo "	Finace Logins "
 			echo " "
-			echo "Bank     | Username : | Passwords : "
-			echo "-------------------------------------- "
-			echo "Payroll  | Username : | Passwords : "
-			echo "------------------------------------- "
-			echo "Benefits | Username : | Passwords : "
-			echo "------------------------------------- "
-			echo "HR       | Username : | Passwords : "
+			echo "Bank     | Username : 38ufh2 | Passwords : 98d258 "
+			echo "-------------------------------------------------- "
+			echo "Payroll  | Username : 3fhhyw | Passwords : 75edrs "
+			echo "-------------------------------------------------- "
+			echo "Benefits | Username : 97F3nf | Passwords : 724s32 "
+			echo "-------------------------------------------------- "
+			echo "HR       | Username : 7ft2h2 | Passwords : 9dght2 "
+	        
+		elif [ $password = Exec123 ] ; then
+			echo "	Executive Logins "
+			echo " "
+			echo "Business    | Username : td8233 | Passwords : 23dok2 "
+			echo "----------------------------------------------------- "
+			echo "Investments | Username : 98dte2 | Passwords : 2o40sw "
+		        echo "----------------------------------------------------- "
+			echo "Partners    | Username : huw672 | Passwords : 46dty7 "
+		       	echo "----------------------------------------------------- "
+			echo "Contacts    | Username : 38dnh7 | Passwords : 09dyu1 "
 		
-	        elif [ $password = Exec321 ] ; then
-			echo " Executive Logins "
+		elif [ $password = Tech123 ] ; then
+			echo "	IT Logins "
 			echo " "
+			echo "Web Server | Username : asd33h | Passwords : 87d52y "
+			echo "---------------------------------------------------- "
+			echo "Databases  | Username : 223hdw | Passwords : 3ff842 "
+			echo "---------------------------------------------------- "
+			echo "Code Repo  | Username : th235d | Passwords : 3f04w2 "
+			echo "---------------------------------------------------- "
+			echo "Email      | Username : 12ed89 | Passwords : wd2r2e "
+		
+		elif [ $password = Ops123 ] ; then
+			echo "	Operation Logins "
 			echo " "
-		elif [ $password = Tech321 ] ; then
-			echo " IT Psswords "
-		elif [ $password = Operation ] ; then
-			echo  " Operation Logins "
+			echo "Building    | Username : 78rot4 | Passwords : i27d23 "
+			echo "----------------------------------------------------- "
+			echo "Waters      | Username : 78tdrw | Passwords : 2ehw24 "
+			echo "----------------------------------------------------- "
+			echo "Electricity | Username : 0d6y2q | Passwords : 36dwg2 "
+			echo "----------------------------------------------------- "
+			echo "Emergency   | Username : 962hw1 | Passwords : 78d32n "
 		fi
 
 
